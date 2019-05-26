cf96041c1bc4cd509fe96771039fa492199fb800
tonbei10 <47322051+tonbei10@users.noreply.github.com>
2019-05-27 00:28:36 +0900

    Update TextToExcel.java

--- src/main/java/com/tonbei/textToexcel/TextToExcel.java
+++ src/main/java/com/tonbei/textToexcel/TextToExcel.java
@@ -12,7 +12,6 @@ import java.nio.file.Paths;
 import java.security.CodeSource;
 import java.security.ProtectionDomain;
 
-import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -21,104 +20,78 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class TextToExcel {
 
-	//適当なディレクトリに書き換えてください
-    public static String INPUT_DIR;
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
+		try {
+			File parepath = getApplicationPath(TextToExcel.class).getParent().toFile();
+			File path = new File(parepath, "Output.xlsx");
 
-        try {
+			Workbook wb = new XSSFWorkbook();
+			FileOutputStream fileOut = new FileOutputStream(path);
 
-        	File parepath = getApplicationPath(TextToExcel.class).getParent().toFile();
-            File path = new File(parepath, "Output.xlsx");
+			for(String arg : args) {
 
-            //xlsxの場合はこちらを有効化
-            Workbook wb = new XSSFWorkbook();
-            FileOutputStream fileOut = new FileOutputStream(path);
+				System.out.println(arg);
 
-            for(String arg : args) {
+				File txt = new File(arg);
+				BufferedReader reader = new BufferedReader(new FileReader(txt));
 
-            	System.out.println(arg);
+				int num = Integer.parseInt(reader.readLine());
+				double x[] = new double[num];
+				double y[] = new double[num];
 
-                File txt = new File(arg);
-                BufferedReader reader = new BufferedReader(new FileReader(txt));
-
-                int num = Integer.parseInt(reader.readLine());
-                double x[] = new double[num];
-                double y[] = new double[num];
-
-                int i = 0;
-                String line;
-    			while ((line = reader.readLine()) != null) {
-    				line = line.replace("  ", " ").replace("  ", " ");
-    				String[] sp = (line.startsWith(" ") ? line.substring(1) : line).split(" ", 2);
-    				x[i] = Double.parseDouble(sp[0]);
-    				y[i] = Double.parseDouble(sp[1]);
-    				i++;
-    				System.out.println(txt.getName() + " : " + i);
-    			}
-    			reader.close();
+				int i = 0;
+				String line;
+				while ((line = reader.readLine()) != null) {
+					line = line.replace("  ", " ").replace("  ", " ");
+					String[] sp = (line.startsWith(" ") ? line.substring(1) : line).split(" ", 2);
+					x[i] = Double.parseDouble(sp[0]);
+					y[i] = Double.parseDouble(sp[1]);
+					i++;
+					System.out.println(txt.getName() + " : " + i);
+				}
+				reader.close();
 
 /*
-                BufferedWriter writer = new BufferedWriter(new FileWriter(new File(parepath, "test.txt")));
-    			for(i = 0; i < num; i++) {
-    				writer.write(x[i] + " " + y[i]);
-    				writer.newLine();
-    			}
-    			writer.close();
+				BufferedWriter writer = new BufferedWriter(new FileWriter(new File(parepath, "test.txt")));
+				for(i = 0; i < num; i++) {
+					writer.write(x[i] + " " + y[i]);
+					writer.newLine();
+				}
+				writer.close();
 */
 
+				String safeName = WorkbookUtil.createSafeSheetName(txt.getName());
+				Sheet sheet1 = wb.createSheet(safeName);
 
-                String safeName = WorkbookUtil.createSafeSheetName(txt.getName());
-                Sheet sheet1 = wb.createSheet(safeName);
+				Row row0 = sheet1.createRow(0);
+				row0.createCell(0).setCellValue("X");
+				row0.createCell(1).setCellValue("Y");
+				for(i = 0; i < num; i++) {
+					Row rowi = sheet1.createRow(i + 1);
+					rowi.createCell(0).setCellValue(x[i]);
+					rowi.createCell(1).setCellValue(y[i]);
+				}
+			}
 
-                CreationHelper createHelper = wb.getCreationHelper();
+			wb.write(fileOut);
+			wb.close();
+			fileOut.close();
 
-                Row row0 = sheet1.createRow(0);
-                row0.createCell(0).setCellValue("X");
-                row0.createCell(1).setCellValue("Y");
-                for(i = 0; i < num; i++) {
-                	Row rowi = sheet1.createRow(i + 1);
-                	rowi.createCell(0).setCellValue(x[i]);
-                	rowi.createCell(1).setCellValue(y[i]);
-                }
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
 
-/*
-                //Rows(行にあたる)を作る。Rowsは0始まり。
-                Row row = sheet1.createRow(0);
-                //cell(列にあたる)を作って、そこに値を入れる。
-                Cell cell = row.createCell(0);
-                cell.setCellValue(1);
+		}
+	}
 
-                row.createCell(1).setCellValue(1.2);
-                row.createCell(2).setCellValue(
-                     createHelper.createRichTextString("sample string"));
-                row.createCell(3).setCellValue(true);
-*/
-
-
-
-            }
-
-            wb.write(fileOut);
-
-            wb.close();
-
-            fileOut.close();
-
-        }catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-
-        }
-    }
-
-    public static Path getApplicationPath(Class<?> cls) throws URISyntaxException {
+	public static Path getApplicationPath(Class<?> cls) throws URISyntaxException {
 		ProtectionDomain pd = cls.getProtectionDomain();
 		CodeSource cs = pd.getCodeSource();
 		URL location = cs.getLocation();
 		URI uri = location.toURI();
 		Path path = Paths.get(uri);
 		return path;
-}
+	}
 
 }
